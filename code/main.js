b669620d537aab4b482d349b486fc68ea8bb39fd
zhengpeiwen <31268592+zhengpeiwen@users.noreply.github.com>
2019-08-16 18:52:42 +0800

    Update main.js

--- code/main.js
+++ code/main.js
@@ -23,18 +23,6 @@ Vue.prototype.signString = function (signData) {
   // 私钥加签   '-----BEGIN PRIVATE KEY-----这里是私钥-----END PRIVATE KEY-----'
   let privateKey = "-----BEGIN PRIVATE KEY-----" +
     "MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAIdPYU5M9/i/0o"
-    + "Vj+gNsGyBh9UyjwDIR5QaVirjXoUahYNIBkk8ON4ieZ+GnXqgqkTnWj0QDlLG88Vtt"
-    + "PZ65he/g+9CgliZcaoCrzXbnIKNQQN/BniwlBHPxh5mlIOwLcD6wknvrjn+b+9Oi1kwh"
-    + "Xz1TzGjRFU/xKW389NqMZ1YrAgMBAAECgYAuF89o8fVC6m1XfUAsrEXtWRFLwz"
-    + "h0lf3zqTtxThslSVIHF/v0LD6krnlquC4ZkS6ZikqRe7rKdTeu0l68VCyYkt71mdNa+dZV"
-    + "O+xvi6m/+Qr+9YUtGu0bGwees/XfUV6oouwFdrbDkA1mVva/IWH9WWcyAUxWOBf"
-    + "vtrLB2tN42QJBANzETldhhhb67vrCWVE0TiVILS+rq7faHolXPjRrWHX/ArWvvkPwKn"
-    + "XPNqbNqAZjRWA9aPDQh7nwCU26f6bUJk8CQQCc56LX1/mqXVAY+9kAJlY2tskP"
-    + "97kyvR8zenij1XlyoP5/vAMxh3D+NlsLl+twnkCLVD/j9QX+qtaUHVlI4vdlAkBLAT8EnR"
-    + "OrlxG+jG1AE59BN2ZyvcaXrjmVu9hcguQJItzO0ai4+E3UvNP6lAC0OuIFMpgGyTJz"
-    + "z8O5btWT3pwtAkAIVYFfx5f6RZSQjyf6iw6/Pzw0veq3WDZFDLdFtHwL66M486qTw"
-    + "ebticOSPRKDW9R/0gzGtegIm9hj658ncO7FAkEAggS+JU62iNHJhSw277CYNvlaiM"
-    + "pg4ZKVwMtzfvqv3TMHbCqfCBxnuEspqWLatSFJUPGctce8POhDWjvxiTTHig=="
     + "-----END PRIVATE KEY-----";
   // 与后端约定好算法 SHA1withRSA  
   let sig = new jsrsasign.KJUR.crypto.Signature({ "alg": "SHA1withRSA", "prov": "cryptojs/jsrsa", "prvkeypem": privateKey });// 初始化的对象 设置好加签或验签的算法和私钥
@@ -50,7 +38,6 @@ Vue.prototype.verify = function (signData, data) {
   // 公钥验签  '-----BEGIN PUBLIC KEY-----这里是公钥-----END PUBLIC KEY-----'
   let publicKey = "-----BEGIN PUBLIC KEY-----" +
     "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCPhDuZsuI7vga1HZvC9cn/Pb1s40CT48iqnyhS8UnLb8k6mfpzBidiJMSpGpaYubW0n9FYuoBCnSd2"
-    + "+IOxwZ5d1XMMWTJ3ujlxL/CWir+V9ZJX23Xcp+vxQ5KWKXtH1v1Y54xhZddf9SqSD5c/GNQqG0w7DS81N3vGPEGYFSUnTQIDAQAB"
     + "-----END PUBLIC KEY-----";
   // 与加签算法一致 SHA1withRSA 
   let sig = new jsrsasign.KJUR.crypto.Signature({ "alg": "SHA1withRSA", "prov": "cryptojs/jsrsa", "prvkeypem": publicKey }); // 初始化的对象 设置好加签或验签的算法和公钥
@@ -65,7 +52,6 @@ Vue.prototype.encrypt = function (signData) {
   // 公钥加密
   let publicKey = "-----BEGIN PUBLIC KEY-----" +
     "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCPhDuZsuI7vga1HZvC9cn/Pb1s40CT48iqnyhS8UnLb8k6mfpzBidiJMSpGpaYubW0n9FYuoBCnSd2"
-    + "+IOxwZ5d1XMMWTJ3ujlxL/CWir+V9ZJX23Xcp+vxQ5KWKXtH1v1Y54xhZddf9SqSD5c/GNQqG0w7DS81N3vGPEGYFSUnTQIDAQAB"
     + "-----END PUBLIC KEY-----";
   let pub = jsrsasign.KEYUTIL.getKey(publicKey); // 获取公钥
   let enc = jsrsasign.KJUR.crypto.Cipher.encrypt(signData, pub); // 加密
@@ -76,16 +62,9 @@ Vue.prototype.encrypt = function (signData) {
 Vue.prototype.decrypt = function (signData) {
   //  解密数据 signData
   // 私钥解密
-  let privateKey = "-----BEGIN PRIVATE KEY-----" + "MIICdQIBADANBgkqhkiG9w0BAQEFAASCAl8wggJbAgEAAoGBAI+EO5my4ju+BrUdm8L1yf89vWzjQJPjyKqfKFLxSctvyTqZ"
-    + "+nMGJ2IkxKkalpi5tbSf0Vi6gEKdJ3b4g7HBnl3VcwxZMne6OXEv8JaKv5X1klfbddyn6/FDkpYpe0fW/VjnjGFl11"
-    + "/1KpIPlz8Y1CobTDsNLzU3e8Y8QZgVJSdNAgMBAAECgYBLDzmstSYlYMlZSFcCrFItMDk2DdTkhCmbMwp/9rpFr/1qrMWUCw"
-    + "/mAJzqZIGG9vnVshBne4NBU7gkPO2DtmQAmAfZkjZWmGXSkiL2tMzq3TUdZtVSVMrWRMft4/VuU1T/GWJsjM/wYwj+x/eWY2AkCs+/vLC2"
-    + "+OlOubrAXmtu8QJBAPklSIWYYqn3"
-    + "/rKtQMqtBZU5S37rj99bqi5vTRIlImjsNm7uASyzjxtLRRqo4pnD4rAm6JU9X8lymIbSwjcxPrMCQQCTdwKyxREKMBj0LzkDHXiMzVzQyz1hyM71IAn"
-    + "+51onYy74l6O3ghAauIn8BhCIGMw4qItoQcCQmWsb+7X7QwH/AkBS2x5SJ4pwYiQXAd0xSfc4WDSOQgK30kE3bhZgeW8HG79Chu5nElP"
-    + "+TLrvkcjCQE3MLTReJWoMkYq+E6s5NaxXAkBLQS/SVXlVrqf9bV/19ANCL8dPEnkXvufG9nvDZwN"
-    + "+GEKvQIufKbPYPxbD08B6A9WskG7cXLnN0U108Wa0fES3AkBlWj7DCpYyYtAAqvpkM+9PBej5OP0beAmxN9reC5dBpinR+MuDypsdEC+gZ"
-    + "/oLcsj2z6mtMlrCt1xVP1I6mIqf" + "-----END PRIVATE KEY-----";
+  let privateKey = "-----BEGIN PRIVATE KEY-----" + 
+      "MIICdQIBADANBgkqhkiG9w0BAQEFAASCAl8wggJbAgEAAoGBAI+EO5my4ju+BrUdm8L1yf89vWzjQJPjyKqfKFLxSctvyTqZ"
+     + "-----END PRIVATE KEY-----";
 
   let prv = jsrsasign.KEYUTIL.getKey(privateKey); // 获取私钥
   signData = jsrsasign.b64tohex(signData); // 将base64 转化为16进制
@@ -102,7 +81,6 @@ Vue.prototype.encrypt1 = function (signData) {
   // 公钥加密
   let publicKey = "-----BEGIN PUBLIC KEY-----" +
     "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCPhDuZsuI7vga1HZvC9cn/Pb1s40CT48iqnyhS8UnLb8k6mfpzBidiJMSpGpaYubW0n9FYuoBCnSd2"
-    + "+IOxwZ5d1XMMWTJ3ujlxL/CWir+V9ZJX23Xcp+vxQ5KWKXtH1v1Y54xhZddf9SqSD5c/GNQqG0w7DS81N3vGPEGYFSUnTQIDAQAB"
     + "-----END PUBLIC KEY-----";
   let encrypt = new JSEncrypt();  // 初始化对象
   encrypt.setPublicKey(publicKey); // 获取公钥
@@ -114,16 +92,9 @@ Vue.prototype.encrypt1 = function (signData) {
 Vue.prototype.decrypt1 = function (signData) {
   //  解密数据 signData
   // 私钥解密
-  let privateKey = "-----BEGIN PRIVATE KEY-----" + "MIICdQIBADANBgkqhkiG9w0BAQEFAASCAl8wggJbAgEAAoGBAI+EO5my4ju+BrUdm8L1yf89vWzjQJPjyKqfKFLxSctvyTqZ"
-    + "+nMGJ2IkxKkalpi5tbSf0Vi6gEKdJ3b4g7HBnl3VcwxZMne6OXEv8JaKv5X1klfbddyn6/FDkpYpe0fW/VjnjGFl11"
-    + "/1KpIPlz8Y1CobTDsNLzU3e8Y8QZgVJSdNAgMBAAECgYBLDzmstSYlYMlZSFcCrFItMDk2DdTkhCmbMwp/9rpFr/1qrMWUCw"
-    + "/mAJzqZIGG9vnVshBne4NBU7gkPO2DtmQAmAfZkjZWmGXSkiL2tMzq3TUdZtVSVMrWRMft4/VuU1T/GWJsjM/wYwj+x/eWY2AkCs+/vLC2"
-    + "+OlOubrAXmtu8QJBAPklSIWYYqn3"
-    + "/rKtQMqtBZU5S37rj99bqi5vTRIlImjsNm7uASyzjxtLRRqo4pnD4rAm6JU9X8lymIbSwjcxPrMCQQCTdwKyxREKMBj0LzkDHXiMzVzQyz1hyM71IAn"
-    + "+51onYy74l6O3ghAauIn8BhCIGMw4qItoQcCQmWsb+7X7QwH/AkBS2x5SJ4pwYiQXAd0xSfc4WDSOQgK30kE3bhZgeW8HG79Chu5nElP"
-    + "+TLrvkcjCQE3MLTReJWoMkYq+E6s5NaxXAkBLQS/SVXlVrqf9bV/19ANCL8dPEnkXvufG9nvDZwN"
-    + "+GEKvQIufKbPYPxbD08B6A9WskG7cXLnN0U108Wa0fES3AkBlWj7DCpYyYtAAqvpkM+9PBej5OP0beAmxN9reC5dBpinR+MuDypsdEC+gZ"
-    + "/oLcsj2z6mtMlrCt1xVP1I6mIqf" + "-----END PRIVATE KEY-----";
+  let privateKey = "-----BEGIN PRIVATE KEY-----" +
+      "MIICdQIBADANBgkqhkiG9w0BAQEFAASCAl8wggJbAgEAAoGBAI+EO5my4ju+BrUdm8L1yf89vWzjQJPjyKqfKFLxSctvyTqZ"
+    + "-----END PRIVATE KEY-----";
 
   let prv = new JSEncrypt(); // 初始化对象
   prv.setPrivateKey(privateKey); // 获取私钥
@@ -132,16 +103,9 @@ Vue.prototype.decrypt1 = function (signData) {
 }
 
 Vue.prototype.signString1 = function (signData) {
-  let privateKey = "-----BEGIN PRIVATE KEY-----" + "MIICdQIBADANBgkqhkiG9w0BAQEFAASCAl8wggJbAgEAAoGBAI+EO5my4ju+BrUdm8L1yf89vWzjQJPjyKqfKFLxSctvyTqZ"
-    + "+nMGJ2IkxKkalpi5tbSf0Vi6gEKdJ3b4g7HBnl3VcwxZMne6OXEv8JaKv5X1klfbddyn6/FDkpYpe0fW/VjnjGFl11"
-    + "/1KpIPlz8Y1CobTDsNLzU3e8Y8QZgVJSdNAgMBAAECgYBLDzmstSYlYMlZSFcCrFItMDk2DdTkhCmbMwp/9rpFr/1qrMWUCw"
-    + "/mAJzqZIGG9vnVshBne4NBU7gkPO2DtmQAmAfZkjZWmGXSkiL2tMzq3TUdZtVSVMrWRMft4/VuU1T/GWJsjM/wYwj+x/eWY2AkCs+/vLC2"
-    + "+OlOubrAXmtu8QJBAPklSIWYYqn3"
-    + "/rKtQMqtBZU5S37rj99bqi5vTRIlImjsNm7uASyzjxtLRRqo4pnD4rAm6JU9X8lymIbSwjcxPrMCQQCTdwKyxREKMBj0LzkDHXiMzVzQyz1hyM71IAn"
-    + "+51onYy74l6O3ghAauIn8BhCIGMw4qItoQcCQmWsb+7X7QwH/AkBS2x5SJ4pwYiQXAd0xSfc4WDSOQgK30kE3bhZgeW8HG79Chu5nElP"
-    + "+TLrvkcjCQE3MLTReJWoMkYq+E6s5NaxXAkBLQS/SVXlVrqf9bV/19ANCL8dPEnkXvufG9nvDZwN"
-    + "+GEKvQIufKbPYPxbD08B6A9WskG7cXLnN0U108Wa0fES3AkBlWj7DCpYyYtAAqvpkM+9PBej5OP0beAmxN9reC5dBpinR+MuDypsdEC+gZ"
-    + "/oLcsj2z6mtMlrCt1xVP1I6mIqf" + "-----END PRIVATE KEY-----";
+  let privateKey = "-----BEGIN PRIVATE KEY-----" + 
+      "MIICdQIBADANBgkqhkiG9w0BAQEFAASCAl8wggJbAgEAAoGBAI+EO5my4ju+BrUdm8L1yf89vWzjQJPjyKqfKFLxSctvyTqZ"
+   + "-----END PRIVATE KEY-----";
 
   let sig = new JSEncrypt(); // 初始化对象
   sig.setPrivateKey(privateKey); // 获取私钥
@@ -153,7 +117,6 @@ Vue.prototype.signString1 = function (signData) {
 Vue.prototype.verify1 = function (signData, data) {
   let publicKey = "-----BEGIN PUBLIC KEY-----" +
     "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCPhDuZsuI7vga1HZvC9cn/Pb1s40CT48iqnyhS8UnLb8k6mfpzBidiJMSpGpaYubW0n9FYuoBCnSd2"
-    + "+IOxwZ5d1XMMWTJ3ujlxL/CWir+V9ZJX23Xcp+vxQ5KWKXtH1v1Y54xhZddf9SqSD5c/GNQqG0w7DS81N3vGPEGYFSUnTQIDAQAB"
     + "-----END PUBLIC KEY-----";
 
   let sig = new JSEncrypt(); // 初始化对象
